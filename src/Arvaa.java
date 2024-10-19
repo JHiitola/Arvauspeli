655353bccb4f4cb5bfe969b209b1e7c79bb2b62a
Jari Antero Hiitola <jari.hiitola@student.hamk.fi>
2024-10-19 16:04:20 +0300

    1. commit

--- src/Arvaa.java
+++ src/Arvaa.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Arvaa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Scanner in = new Scanner(System.in);
+        String rightName = "Toivo";
+        String input;
+        String guess = "";
+        int tries = 0;
+        boolean stop = false;
+
+        while (true) {
+            System.out.println("Guess my name (if you want to quit though, type 'stop')\n");
+            guess = in.nextLine();
+            tries++;
+
+            if (guess.equalsIgnoreCase("stop")) {
+                stop = true;
+                break;
+            }
+        }
     }
 }
